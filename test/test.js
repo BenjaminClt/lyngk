e0bcc605f6fb030d26e0262720d7780e7950ec09
Benjamin Cléton <benjamin.cleton@hotmail.com>
2017-10-09 10:38:33 +0200

    test Histoire 2

--- test/test.js
+++ test/test.js
@@ -5,6 +5,24 @@ var LyngkTestCase = TestCase("LyngkTestCase");
 LyngkTestCase.prototype.testCoordinate = function()
 {
     var testCoord = new Lyngk.Coordinates('A', 1);
-
     assertFalse(testCoord.isValid());
+};
+
+LyngkTestCase.prototype.test43ValidCoordinate = function()
+{
+    var count = 0;
+    var letters = "ABCDEFGHI";
+
+    for (var i = 0; i < letters.length; i++)
+    {
+        for (var j = 1; j < 10; j++)
+        {
+            var tmpCoord = new Lyngk.Coordinates(letters[i], j);
+
+            if (tmpCoord.isValid())
+                count++;
+        }
+    }
+
+    assertEquals(count, 43);
 };
