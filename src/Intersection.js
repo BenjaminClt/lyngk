a1457d7adca64d6dec90a5095e1923451ad71bf7
Benjamin Cléton <benjamin.cleton@hotmail.com>
2017-10-09 12:07:02 +0200

    Histoire 10 setPiece

--- src/Intersection.js
+++ src/Intersection.js
@@ -4,24 +4,25 @@ Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
 Lyngk.Intersection = function (c) {
     var state = Lyngk.State.VACANT;
-    var piece;
+    var pieces = [];
 
     this.getState = function(){
         return state;
     };
 
     this.setPiece = function(p){
-        if (piece != null)
-            state = Lyngk.State.STACK;
-        else
+        pieces.push(p);
+        if (pieces.length == 1)
             state = Lyngk.State.ONE_PIECE;
-
-        piece = p;
+        else if (pieces.length >= 5)
+            state = Lyngk.State.FULL_STACK;
+        else
+            state = Lyngk.State.STACK;
     };
 
     this.getColor = function(){
-        if (piece != null)
-            return piece.getColor();
+        if (pieces.length > 0)
+            return pieces[pieces.length-1].getColor();
         else
             return null;
     };
