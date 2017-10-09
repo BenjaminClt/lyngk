140018d7dc4206a4ab1b0d2e820f745ecc8b3dc4
Benjamin Cléton <benjamin.cleton@hotmail.com>
2017-10-09 10:26:46 +0200

    Creation fonction isValid

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -13,7 +13,7 @@ Lyngk.Coordinates = function (c, l) {
         ,'G3','G4','G5','G6','G7','G8','G9'
         ,'H5','H6','H7','H8'
         ,'I7'];
-
+    
     this.isValid = function (){
         return tab.indexOf(col + ligne) != -1;
     };
