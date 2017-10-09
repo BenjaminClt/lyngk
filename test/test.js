1b8d366fb4c5a0bff61fc4c135b87a47173eb761
Benjamin Cléton <benjamin.cleton@hotmail.com>
2017-10-09 10:13:17 +0200

    Creation fonction testCoordinate

--- test/test.js
+++ test/test.js
@@ -1,3 +1,10 @@
 'use strict';
 
 var LyngkTestCase = TestCase("LyngkTestCase");
+
+LyngkTestCase.prototype.testCoordinate = function()
+{
+    var testCoord = new Lyngk.Coordinates('A', 1);
+
+    assertFalse(testCoord.isValid());
+};
