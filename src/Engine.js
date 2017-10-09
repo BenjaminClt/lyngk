8b73519cbe86e3f32861c79b6479906d563d42ca
Benjamin Cléton <benjamin.cleton@hotmail.com>
2017-10-09 12:45:13 +0200

    Histoire 11 isCorrectlyInitialized

--- src/Engine.js
+++ src/Engine.js
@@ -4,4 +4,32 @@
 Lyngk.Color = {BLACK: 0, IVORY: 1, BLUE: 2, RED: 3, GREEN: 4, WHITE: 5};
 
 Lyngk.Engine = function () {
+    var intersections = [];
+
+    var init = function(){
+        var size = Lyngk.AllCoordinates.length;
+
+        for (var i = 0; i < size; i++)
+        {
+            var inter = new Lyngk.Intersection(653);
+            inter.setPiece(new Lyngk.Piece(Lyngk.Color.WHITE));
+            intersections.push(inter);
+        }
+    };
+
+    this.isCorrectlyInitialized = function()
+    {
+        if (intersections.length != Lyngk.AllCoordinates.length)
+            return false;
+
+        for (var i = 0; i < intersections.length; i++)
+        {
+            if (intersections[i].getState() !== Lyngk.State.ONE_PIECE)
+                return false;
+        }
+
+        return true;
+    };
+
+    init();
 };
